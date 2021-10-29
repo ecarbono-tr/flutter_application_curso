559faffd4619b382d869aa87c9a300d51997d64e
ecarbono <ecarbono8@gmail.com>
2021-10-29 14:11:55 -0500

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,49 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({
-    Key? key,
-  }) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: ScreanRow(img1: imgtop),
-                ),
-                Expanded(
-                  child: ScreanRow(img1: imgmiddle),
-                ),
-                Expanded(
-                  child: ScreanRow(img1: imgBotton),
-                ),
-              ],
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 50),
+            child: Center(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ScreanRow(img1: imgtop),
+                  ),
+                  Expanded(
+                    child: ScreanRow(img1: imgmiddle),
+                  ),
+                  Expanded(
+                    child: ScreanRow(img1: imgBotton),
+                  ),
+                ],
+              ),
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          elevation: 2,
+          onPressed: () {
+            setState(() {});
+          },
+          child: const Icon(
+            Icons.refresh,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.green,
         ),
       ),
     );
